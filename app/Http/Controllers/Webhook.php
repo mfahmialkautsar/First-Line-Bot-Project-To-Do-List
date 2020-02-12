6d410e11bc66da8074626b408739dd9a280908d6
Fahmi Al <mfahmiak@outlook.com>
2020-02-12 09:19:55 +0700

    commit

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -193,7 +193,7 @@ class Webhook extends Controller
             $list = array("Here's what you should remember:");
             for ($i = 0; $i < $total; $i++) {
                 $memory = $this->memoryGateway->getMemory($tableName, $i + 1);
-                if (!$memory)
+                if ($memory == null)
                     continue;
                 array_push($list, $i + 1 . ". " . $memory['remember']);
             }
