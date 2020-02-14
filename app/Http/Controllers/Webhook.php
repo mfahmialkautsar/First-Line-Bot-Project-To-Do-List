ce8aa11a52a75cfeced6902a6c44a2e07a2314ca
Fahmi Al <mfahmiak@outlook.com>
2020-02-14 10:55:59 +0700

    publish ready

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -293,7 +293,7 @@ class Webhook extends Controller
                             for ($i = 0; $i < $deleteCount; $i++) {
                                 // extract int
                                 preg_match_all('!\d+!', $words[$i], $result);
-                                if ($result[0][0]) {
+                                if (isset($result[0][0])) {
                                     if (($result[0][0] == $words[$i])) {
                                         // if (is_int($deleteList)) {
                                         $message = "passed";
